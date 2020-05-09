48c4dafda14af5a636be6033962027d059298bae
nareshddevops <65061652+nareshddevops@users.noreply.github.com>
2020-05-09 12:43:02 +0530

    Create jsonupdate.py

---
+++ nodeapp/jsonupdate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys, json
+
+tmp_file = open("config.json", "r")
+json_object = json.load(tmp_file)
+tmp_file.close()
+
+if not json_object['user']:
+    json_object["user"] = sys.argv[1]
+if not json_object["api_key"]:
+    json_object["api_key"] = sys.argv[2]
+if not json_object["conn_string"]:
+    json_object["conn_string"] = sys.argv[3]
+if not json_object["ip_address"]:
+    json_object["ip_address"] = sys.argv[4]
+
+tmp_file = open("config.json", "w")
+json.dump(json_object, tmp_file)
+tmp_file.close()
