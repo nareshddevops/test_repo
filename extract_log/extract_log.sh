401c1359d3c195fd41e66867a3a69106531fd5a4
nareshddevops <65061652+nareshddevops@users.noreply.github.com>
2020-05-09 12:10:19 +0530

    Create extract_log.sh

---
+++ extract_log/extract_log.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+FROM_DATE=""
+TO_DATE=""
+LOG_LEVEL="SEVERE"
+LOGS_DIR="/logs"
+
+#read -p 'Enter log level: ' LOG_LEVEL
+#read -p 'Enter log files directory (absolute path): ' LOGS_DIR
+#read -p 'Enter log from timestamp: ' FROM_DATE
+#read -p 'Enter log to timestamp: ' TO_DATE
+
+echo > /extractedlog.txt
+for file in $(find $LOGS_DIR -type f); do 
+    grep "$LOG_LEVEL" $file | awk '$0>=from && $0<=to' from="$FROM_DATE" to="$TO_DATE" | awk -v prefix="$file" '{print prefix $0}' >> /extractedlog.txt
+done
