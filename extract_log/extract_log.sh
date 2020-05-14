273e4842a4401c27cb91aee0e96056a7ee1a96a5
nareshddevops <65061652+nareshddevops@users.noreply.github.com>
2020-05-14 19:19:05 +0530

    Update extract_log.sh

--- extract_log/extract_log.sh
+++ extract_log/extract_log.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-FROM_DATE=""
-TO_DATE=""
+FROM_DATE="May 08, 2020 19:00:00"
+TO_DATE="May 09, 2020 09:00:00"
 LOG_LEVEL="SEVERE"
 LOGS_DIR="logs"
 
@@ -10,7 +10,7 @@ LOGS_DIR="logs"
 #read -p 'Enter log from timestamp: ' FROM_DATE
 #read -p 'Enter log to timestamp: ' TO_DATE
 
-echo > /extractedlog.txt
-for file in $(find $LOGS_DIR -type f); do 
-    grep "$LOG_LEVEL" $file | awk '$0>=from && $0<=to' from="$FROM_DATE" to="$TO_DATE" | awk -v prefix="$file" '{print prefix $0}' >> /test.txt
+echo > /home/testuser/test.txt
+for file in $(find $LOGS_DIR -type f); do
+    grep "$LOG_LEVEL" $file | awk '$0>=from && $0<=to' from="$FROM_DATE" to="$TO_DATE" | awk -v prefix="$file " '{print prefix $0}' >> /home/testuser/test.txt
 done
