2995d87cf85b02f6a642f9a7afe7fdd74ec9b350
nareshddevops <65061652+nareshddevops@users.noreply.github.com>
2020-05-14 19:18:01 +0530

    Update nodeapp.sh

--- nodeapp/nodeapp.sh
+++ nodeapp/nodeapp.sh
@@ -1,37 +1,35 @@
 #!/bin/sh
 
-GIT_REPO_URL = ""
-USER = ""
-API_KEY = ""
-CONNECTION_STRING = ""
-IP_ADDRESS = ""
+GIT_REPO_URL="https://github.com/nareshddevops/nodeapp.git"
+USER="testuser"
+API_KEY="4567"
+CONNECTION_STRING="remote.host.com:8080"
+IP_ADDRESS="remote.host.com"
 
 # checkout code from github
-mkdir -p /home/testuser/mycode/
-cd /home/testuser/mycode/
-git clone $GIT_REPO_URL
+cd /home/testuser/
+git clone $GIT_REPO_URL mycode
 
 # Update config.json with variables
-/usr/bin/python jsonupdate.py $USER $API_KEY $CONNECTION_STRING $IP_ADDRESS
+/usr/bin/python3 /home/testuser/code/nodeapp/jsonupdate.py $USER $API_KEY $CONNECTION_STRING $IP_ADDRESS
 # Change the ownership of the code directory
-chown -R testuser /home/testuser/mycode
+chown -R testuser:testuser /home/testuser/mycode
 # Archive folder and scp to remote machine
-tar -czf app.tar.gz /home/testuser/mycode/
+tar -czf app.tar.gz -C mycode .
 md5sum app.tar.gz > appfiles.md5
-scp app.tar.gz appfiles.md5 testuser@remote.test.com:/home/testuser/remotecode/
+
+scp app.tar.gz appfiles.md5 testuser@remote.test.com:/home/testuser/remotecode
 
 # Execute the commands remotely
-ssh testuser@remote.test.com << EOF
-    systemctl stop node
+ssh -T testuser@remote.test.com << EOF
+    sudo systemctl stop node
     mv /home/testuser/remotecode/app.* /user/node/data/
+    cd /user/node/data/
     tar -xf app.tar.gz
-    systemctl start node
+    sudo systemctl start node
 EOF
 
 # Check the status code
-status=$(curl --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null ​http://remote.test.com/status)
-if [[ "$status" -eq "200" ]] ; then
-  echo "status : 200"
-else
-  ssh testuser@remote.test.com "systemctl restart node" 
-fi
+sleep 1s
+status="$(curl --connect-timeout 5 --write-out %{http_code} --silent --output /dev/null http://remote.test.com/status)"
+echo "Status : $status"
